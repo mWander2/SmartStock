d31a672ec9663bca5651dba4ab817b795e728005
Michael Wander <michaelwander0@gmail.com>
2023-06-01 19:55:35 -0400

    MyGamesView

--- final-capstone/capstone/vue/src/views/MyGamesView.vue
+++ final-capstone/capstone/vue/src/views/MyGamesView.vue
@@ -3,6 +3,7 @@
     <h2>My Games</h2>
     <div class="game-container" v-for="game in gamesList" v-bind:key="game.gameId">
         <h2>{{game.gameName}}</h2>
+        <p>{{game}}</p>
         <router-link v-bind:to="{name: 'view-game', params: {id: game.gameId}}">
             View Game
         </router-link>
@@ -20,12 +21,17 @@ export default {
         }
     },
     created() {
-        stockService.showMyGames().then(
+        stockService.getAllGames().then(
             (response) => {
                 this.gamesList = response.data;
 
             }
         )
+        // stockService.showMyGames().then(
+        //     response => {
+        //         this.gamesList = response.data
+        //     }
+        // )
     }
     
 }
