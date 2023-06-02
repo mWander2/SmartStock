32d184a9f76313277eb72e43d8b3a32203a797e3
Lucy Conner <lucyaconner@gmail.com>
2023-06-02 09:12:25 -0600

    fix stockcontroller

--- final-capstone/capstone/java/src/main/java/com/techelevator/api/model/StockModel.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/api/model/StockModel.java
@@ -1,16 +1,15 @@
 package com.techelevator.api.model;
 
-<<<<<<< HEAD
 import java.util.List;
 
 public class StockModel {
-        private boolean adjusted;
-        private int queryCount;
-        private String requestId;
-        private int resultsCount;
-        private String status;
-        private String stockTicker;
-        private List<ResultsModel> results;
+    private boolean adjusted;
+    private int queryCount;
+    private String requestId;
+    private int resultsCount;
+    private String status;
+    private String stockTicker;
+    private List<ResultsModel> results;
 
     public StockModel(boolean adjusted, int queryCount, String requestId, int resultsCount, String status, String ticker, List<ResultsModel> results) {
         this.adjusted = adjusted;
@@ -76,31 +75,5 @@ public class StockModel {
 
     public void setResults(List<ResultsModel> results) {
         this.results = results;
-=======
-import java.math.BigDecimal;
-
-public class StockModel {
-
-    private BigDecimal currentPrice;
-    private String ticker;
-    private String stockName;
-
-    public void ApiModel(String stockName, String ticker, BigDecimal currentPrice){
-        this.currentPrice = currentPrice;
-        this.stockName = stockName;
-        this.ticker = ticker;
-    }
-
-    public BigDecimal getCurrentPrice() {
-        return currentPrice;
-    }
-
-    public String getTicker() {
-        return ticker;
-    }
-
-    public String getStockName() {
-        return stockName;
->>>>>>> main
     }
 }
