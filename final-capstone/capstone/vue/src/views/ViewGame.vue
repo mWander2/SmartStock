df67375b732e2cad49d4e159b1b6ac0b2281d7d6
Michael Wander <michaelwander0@gmail.com>
2023-06-02 11:35:08 -0400

    Working on Leaderboard and ViewGame

--- final-capstone/capstone/vue/src/views/ViewGame.vue
+++ final-capstone/capstone/vue/src/views/ViewGame.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>
-    <game />
+  <div class ="view-game">
+    <game class="game"/>
+    <leaderboard class="leaderboard"/>
   </div>
 </template>
 
 <script>
 import Game from '../components/Game.vue'
+import Leaderboard from '../components/Leaderboard.vue'
 export default {
   components: {
     Game,
+    Leaderboard
+
   }
 }
 </script>
 
-<style>
+<style scope>
+
+.view-game {
+  display: flex;
+  flex-direction: column;
+}
+
+.game {
+  height: 40vh;
+}
 
 </style>
